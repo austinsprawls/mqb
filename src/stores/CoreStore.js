3d8707ad1669fb599246548a2f35a79b6f244de0
Austin Sprawls <austinsprawls@Opss-MacBook-Pro-4.local>
2015-10-22 16:23:31 -0500

    added a core store and started working on listing out vehicles in a simple unordered list; Promise doesn't seem to be working

---
+++ src/stores/CoreStore.js
@@ -0,0 +1,45 @@
+/**
+ * Created by austinsprawls on 10/22/15.
+ */
+"use strict";
+
+import Dispatcher from '../core/Dispatcher';
+import ActionTypes from '../constants/ActionTypes';
+var EventEmitter = require('events').EventEmitter;
+var CHANGE_EVENT = 'change';
+
+var _state= '';
+
+
+var CoreStore = Object.assign({}, EventEmitter.prototype, {
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+
+  getState: function() {
+    return _state;
+  }
+
+});
+
+Dispatcher.register(function(action) {
+  switch(action.actionType) {
+    case ActionTypes.INITIALIZE:
+      _state = action.initialData.state;
+      CoreStore.emitChange();
+      break;
+    default:
+    // no op
+
+  }
+});
+
+export default CoreStore;
