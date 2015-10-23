fcc40f53bde20dd2aaac761a73d5d0f48ef7de56
Justin Lilly <justin.lilly@clutchanalytics.com>
2015-10-23 10:20:30 -0500

    remove un-used code

--- src/utils/restUtil.js
+++ src/utils/restUtil.js
@@ -46,21 +46,3 @@ function restUtil(){
   };
 
 }
-
-{
-  quote: {
-    create: () => {
-      return new Promise(  (success, error) => {
-          var options = {
-            url: "/api/quotes/",
-            method: "POST",
-            dataType:"json",
-            success: success,
-            error: error
-          };
-          $.ajax(options);
-        }
-      )
-    }
-  }
-};
